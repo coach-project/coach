a172dca13adb62514a4f9e5a10716f509ae7f8a7
neerajsohal <hello@neerajkumar.name>
2013-12-04 01:05:07 +0530

    ditched interfaces over abstracts

--- src/Coach/Scm/Git/Git.php
+++ src/Coach/Scm/Git/Git.php
@@ -2,9 +2,7 @@
 
 namespace Coach\Scm\Git;
 
-use Coach\Scm\ScmInterface;
-
-class Git implements ScmInterface {
+class Git extends ScmAbstract {
 
 	public function cloneRepository($url) {
 		return "git clone " . $url;
